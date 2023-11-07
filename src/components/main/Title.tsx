b0f980b6c183acefab51a26307f49528fd433655
2hanbyeol1 <2hanbyeol1@naver.com>
2023-11-08 00:32:00 +0900

    initial setting

---
+++ src/components/main/Title.tsx
@@ -0,0 +1,5 @@
+const Title: React.FC = () => {
+  return <div>챗챗💬</div>;
+};
+
+export default Title;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import Title from "../components/main/Title";
+
+export default function Main() {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+}
